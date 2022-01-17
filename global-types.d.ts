b8d877d7535e780e81980d8495634a098f9cbec8
jkoenig <julian.koenig@js-soft.com>
2022-01-17 17:05:03 +0100

    chore: add dev-env

---
+++ global-types.d.ts
@@ -0,0 +1,52 @@
+import * as _JSSNative from "@js-soft/native-web"
+import * as _NMSHDAppRuntime from "@nmshd/app-runtime"
+import * as _NMSHDContent from "@nmshd/content"
+import * as _NMSHDCrypto from "@nmshd/crypto"
+import * as _NMSHDRuntime from "@nmshd/runtime"
+import * as _NMSHDTransport from "@nmshd/transport"
+import * as _tsLogger from "./typings/logger"
+
+declare global {
+    const NMSHDRuntime: typeof _NMSHDRuntime
+    const NMSHDAppRuntime: typeof _NMSHDAppRuntime
+    const NMSHDContent: typeof _NMSHDContent
+    const JSSNative: typeof _JSSNative
+
+    const runtime: _NMSHDAppRuntime.AppRuntime
+    const bootstrapper: _JSSNative.INativeBootstrapper
+    const _bootstrapPromise: Promise
+    const QRCode: any
+    const App: any
+
+    const appLogger: _tsLogger.ILogger
+    const NMSHDTransport: typeof _NMSHDTransport
+
+    /*
+    const NMSHDTransport: {
+        CoreDate: typeof _NMSHDTransport.CoreDate
+        CoreId: typeof _NMSHDTransport.CoreId
+        CoreAddress: typeof _NMSHDTransport.CoreAddress
+    }
+    */
+    const NMSHDCrypto: {
+        CoreBuffer: typeof _NMSHDCrypto.CoreBuffer
+        Encoding: typeof _NMSHDCrypto.Encoding
+    }
+
+    interface Window {
+        NMSHDRuntime: typeof _NMSHDRuntime
+        NMSHDAppRuntime: typeof _NMSHDAppRuntime
+        NMSHDContent: typeof _NMSHDContent
+        JSSNative: typeof _JSSNative
+        runtime: _NMSHDAppRuntime.AppRuntime
+        bootstrapper: _JSSNative.INativeBootstrapper
+        _bootstrapPromise: Promise
+        App: any
+        QRCode: any
+        appLogger: _tsLogger.ILogger
+    }
+
+    interface JQueryStatic {
+        sap: any
+    }
+}
