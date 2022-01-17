4a8c38f36af80109a1faf8d3110dc597c950fa90
jkoenig <julian.koenig@js-soft.com>
2022-01-17 17:28:54 +0100

    ci: add pipeline

---
+++ .ci/runChecks.sh
@@ -0,0 +1,8 @@
+set -e
+set -x
+
+# npm ci
+npm run lint:prettier
+npm run lint:eslint
+npx license-check --ignoreLicenses "(MIT OR GPL-3.0-or-later)"
+npx better-npm-audit audit --exclude 1004876,1006865,1006864,1006883
