dcb99ea8b183344858c46baaf5db07dd4bf6529f
jkoenig <julian.koenig@js-soft.com>
2022-01-18 08:58:24 +0100

    fix: correct paths in version bump script

--- _tools/versionBump.js
+++ _tools/versionBump.js
@@ -1,21 +1,20 @@
 const fs = require("fs")
 const prompt = require("prompt")
 
-function updatePackageJsons(newVersion) {
-    const files = ["package.json"]
-    files.forEach((file) => {
-        const json = JSON.parse(fs.readFileSync(file))
-        json.version = newVersion
-        fs.writeFileSync(file, JSON.stringify(json, null, 4))
-        console.info(`bumped ${file} to ${newVersion}`)
-    })
+function updatePackageJson(newVersion) {
+    const file = `${__dirname}/../package.json`
+
+    const json = require(file)
+    json.version = newVersion
+    fs.writeFileSync(file, `${JSON.stringify(json, null, 4)}\n`)
+    console.info(`bumped ${file} to ${newVersion}`)
 }
 
 function updateManifestJson(newVersion) {
     const file = "webapp/manifest.json"
     const json = JSON.parse(fs.readFileSync(file))
     json["sap.app"].applicationVersion.version = newVersion
-    fs.writeFileSync(file, JSON.stringify(json, null, 4))
+    fs.writeFileSync(file, `${JSON.stringify(json, null, 4)}\n`)
     console.info(`bumped ${file} to ${newVersion}`)
 }
 
@@ -24,7 +23,7 @@ prompt.get(
     {
         properties: {
             version: {
-                description: `enter new version - current: (${require("./package.json")["version"]})`,
+                description: `enter new version - current: (${require(`${__dirname}/../package.json`)["version"]})`,
                 type: "string",
                 pattern: /^([0-9]|[1-9][0-9]).([0-9]|[1-9][0-9]).([0-9]|[1-9][0-9])?$/,
                 message: 'not in semantic version format: "xx.xx.xx"',
@@ -38,7 +37,7 @@ prompt.get(
         }
 
         const newVersion = result.version
-        updatePackageJsons(newVersion)
+        updatePackageJson(newVersion)
         updateManifestJson(newVersion)
     }
 )
