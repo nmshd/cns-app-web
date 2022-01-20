b1ae199560bd29b6ddd5ff0c1701394adedf1f7d
Jonas Gann <jonas@gann.digital>
2022-01-20 10:40:05 +0100

    Bugfix/jssnmshd-2808-google-apis (#3)
    * fix: allow access to google api
    
    * chore: bump version
    
    * fix: dependency version
    
    * fix: update audit excludes
    
    * fix: update package-lock
    
    * chore: update package-lock

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -5,4 +5,4 @@ npm ci
 npm run lint:prettier
 npm run lint:eslint
 npx license-check --ignoreLicenses "(MIT OR GPL-3.0-or-later)"
-npx better-npm-audit audit --exclude 1004876,1006865,1006864,1006883
+npx better-npm-audit audit --exclude 1004876,1006883,1006886
