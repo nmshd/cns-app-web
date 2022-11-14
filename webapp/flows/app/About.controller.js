5b77c65465579690df1e76f12398a1d9572efcc6
Ali Ince <ince.ali@msn.com>
2022-11-14 14:23:22 +0100

    fix: remove static text
    Co-authored-by: Simon Coen <Siolto@users.noreply.github.com>

--- webapp/flows/app/About.controller.js
+++ webapp/flows/app/About.controller.js
@@ -6,7 +6,7 @@ sap.ui.define(["nmshd/app/core/App", "nmshd/app/core/_AppController"], (App, Acc
         onRouteMatched(oEvent) {
             App.appController.setLeft("sap-icon://nav-back", null)
             App.appController.clearRight()
-            App.appController.setTitle("Über die App")
+            App.appController.setTitle(this.resource("master.about"))
             this.super("onRouteMatched", oEvent)
         },
 
