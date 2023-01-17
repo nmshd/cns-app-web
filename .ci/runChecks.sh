7bda2c4a018f8174ce0c81c077c6eb2fcb6cfc69
Julian König <33655937+jkoenig134@users.noreply.github.com>
2023-01-17 14:07:27 +0100

    Chore/bump app runtime (#64)
    * chore: bump natives
    
    * chore: set app-web version to 2.0.11
    
    * chore: bump app-runtime

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -5,4 +5,4 @@ npm ci
 npm run lint:prettier
 npm run lint:eslint
 npx license-check --ignoreLicenses "(MIT OR GPL-3.0-or-later)"
-npx better-npm-audit audit --exclude 1085410,1088468
+npx better-npm-audit audit
