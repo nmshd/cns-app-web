a20147d38cf4d33f306ed76c625fb11abe20de6a
Stnmtz <t.stnmtz@outlook.de>
2023-02-18 22:26:44 +0100

    feat: have account selection popup for multiple profiles

--- webapp/flows_phone/app/App.controller.js
+++ webapp/flows_phone/app/App.controller.js
@@ -1,6 +1,6 @@
 sap.ui.define(
-    ["nmshd/app/core/App", "nmshd/app/core/_AppController", "nmshd/app/core/Event"],
-    (App, BaseController, Event) => {
+    ["nmshd/app/core/App", "nmshd/app/core/_AppController", "nmshd/app/core/Event", "sap/ui/core/Fragment"],
+    (App, BaseController, Event, Fragment) => {
         "use strict"
         return BaseController.extend("nmshd.app.flows_phone.app.App", {
             createViewModel() {
@@ -19,7 +19,10 @@ sap.ui.define(
                     },
                     leftIcon: "",
                     rightIcon: "",
-                    styleClass: "appAccounts"
+                    styleClass: "appAccounts",
+                    accountSelectionTitle: "",
+                    accountSelectionDescription: "",
+                    accountSelectionAccounts: []
                 }
             },
             clear() {},
@@ -240,6 +243,83 @@ sap.ui.define(
             },
             toProfile() {
                 App.navTo("account.home", "account.attributes", { accountId: App.accountId() })
+            },
+            async openAccountSelectionPopup(accounts, title, description) {
+                const oView = this.getView()
+                this.viewProp("/accountSelectionAccounts", accounts ? accounts : [])
+                this.viewProp("/accountSelectionTitle", title)
+                this.viewProp("/accountSelectionDescription", description)
+
+                const that = this
+                if (!that.popups) {
+                    that.popups = {}
+                }
+                const type = "AccountSelectionPopup"
+                if (!that.popups[type]) {
+                    const oDialog = await Fragment.load({
+                        id: oView.getId(),
+                        name: "nmshd.app.flows.accounts." + type,
+                        controller: this
+                    })
+                    oView.addDependent(oDialog)
+                    oDialog.open()
+                    that.popups[type] = oDialog
+                    that.currentPopup = oDialog
+                } else {
+                    that.popups[type].open()
+                    that.currentPopup = that.popups[type]
+                }
+            },
+            closeAccountSelectionPopup() {
+                if (this.currentPopup) {
+                    this.currentPopup.close()
+                    this.currentPopup = null
+                }
+            },
+
+            onAccountSelectionPress(oEvent) {
+                try {
+                    const oItem = oEvent.getParameter("listItem") || oEvent.getSource()
+                    const prop = oItem.getBindingContextPath()
+                    const selectedAccount = this.viewProp(prop)
+                    appLogger.info(`User chose ${selectedAccount.name} with id ${selectedAccount.id}.`)
+                    if (App.accountSelectionCallback) {
+                        App.accountSelectionCallback(selectedAccount)
+                    }
+                } catch (e) {
+                    App.error(e)
+                } finally {
+                    this.closeAccountSelectionPopup()
+                }
+            },
+
+            async onAccountSelectionCreate() {
+                try {
+                    appLogger.info("User decided for a new account to be created for an accountSelectionRequest.")
+                    const accounts = await runtime.accountServices.getAccounts()
+                    const accountname =
+                        this.resource("accounts.processRelationshipToken.profile") + (accounts.length + 1)
+                    const oAccounts = await runtime.accountServices.createAccount(
+                        NMSHDTransport.Realm.Prod,
+                        accountname
+                    )
+                    this.localAccount = oAccounts
+                    await App.selectAccount(this.localAccount.id, "")
+                    appLogger.info(`Account ${this.localAccount.id} was created for account selection.`)
+                    if (App.accountSelectionCallback) {
+                        App.accountSelectionCallback(this.localAccount)
+                    }
+                } catch (e) {
+                    App.error(e)
+                } finally {
+                    this.closeAccountSelectionPopup()
+                }
+            },
+            onAccountSelectionClose() {
+                this.closeAccountSelectionPopup()
+                if (App.accountSelectionCallback) {
+                    App.accountSelectionCallback()
+                }
             }
         })
     }
