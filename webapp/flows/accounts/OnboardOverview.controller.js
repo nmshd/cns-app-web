7a75c0578317a98e153621a221ce66e985426a79
Stnmtz <t.stnmtz@outlook.de>
2023-04-28 12:24:28 +0200

    chore: design changes overview

--- webapp/flows/accounts/OnboardOverview.controller.js
+++ webapp/flows/accounts/OnboardOverview.controller.js
@@ -7,7 +7,8 @@ sap.ui.define(
             createViewModel() {
                 return {
                     busy: false,
-                    delay: 0
+                    delay: 0,
+                    submitAvailable: true
                 }
             },
 
@@ -15,19 +16,18 @@ sap.ui.define(
                 this.navTo(oEvent.getParameter("listItem").data("key"))
             },
 
-            async onInitialized() {
-                
-            },
+            async onInitialized() {},
 
             async onRouteMatched(oEvent) {
                 App.setMenuIcon()
-                App.appController.setTitle("Enmeshed")
+                App.appController.setTitle(this.resource("accounts.create.title"))
                 await App.isInitialized()
                 const aAccounts = await App.localAccountController().getAccounts()
                 if (aAccounts.length >= 1) {
                     App.setMenuIcon()
                 }
                 await this.super("onRouteMatched")
+                App.appController.setTitle(this.resource("accounts.create.title"))
 
                 this.clear()
             },
@@ -47,7 +47,32 @@ sap.ui.define(
             },
 
             onNoCode() {
-                this.navTo("accounts.onboardnocode")
+                //this.navTo("accounts.onboardnocode")
+                this.create()
+            },
+
+            async create() {
+                try {
+                    this.loadInc()
+                    this.viewProp("/submitAvailable", false)
+                    const accounts = await runtime.accountServices.getAccounts()
+                    const accountname =
+                        this.resource("accounts.processRelationshipToken.profile") + (accounts.length + 1)
+                    const oAccounts = await runtime.accountServices.createAccount(
+                        NMSHDTransport.Realm.Prod,
+                        accountname
+                    )
+                    this.localAccount = oAccounts
+                    await App.selectAccount(this.localAccount.id, "")
+                    this.accountId = this.localAccount.id
+                    App.navTo("account.login", "account.home", {
+                        accountId: this.accountId
+                    })
+                } catch (e) {
+                    App.error(e)
+                } finally {
+                    this.loadDec()
+                }
             },
 
             toEULA() {
