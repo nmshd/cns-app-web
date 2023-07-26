6a397451d955cc6713a56b1d6e80a28892d905be
Siolto <simon.coen@js-soft.com>
2023-07-26 11:22:11 +0200

    refactor: refactor existing attributechange coding

--- src/core/popups/AttributeChangePopup.controller.ts
+++ src/core/popups/AttributeChangePopup.controller.ts
@@ -1,3 +1,5 @@
+import List from "sap/m/List"
+import StandardListItem from "sap/m/StandardListItem"
 import JSONModel from "sap/ui/model/json/JSONModel"
 import PopupController from "./PopupController"
 import { PopupType } from "./PopupType"
@@ -7,37 +9,39 @@ import { PopupType } from "./PopupType"
  */
 export default class AttributeChangePopupController extends PopupController {
     popupType = PopupType.AttributeChangePopup
-    private attributeId?: string
 
     public async refresh() {
-        this.attributeId = undefined
-        if (!this.params.data || !this.params.data.attributeId) {
+        if (!this.params.data || !this.params.data.selectedItemPath || !this.params.data.query) {
             appLogger.warn("No/wrong data for AttributeChangePopup")
             return
         }
-        this.attributeId = this.params.data.attributeId
-        if (!this.attributeId) return
 
-        const attributeResult = await runtime.currentSession.consumptionServices.attributes.getAttribute({
-            id: this.attributeId
-        })
-        if (attributeResult.isError) {
-            App.error(attributeResult.error)
-            return
-        }
-        const expandedAttribute = await runtime.currentSession.expander.expandLocalAttributeDTO(attributeResult.value)
-
-        const attributeModel = new JSONModel(expandedAttribute)
+        const attributeModel = new JSONModel(this.params.data.query)
         this.view?.setModel(attributeModel)
-        this.view?.bindElement("/")
+
+        this.setSelectedItem(this.params.data.selectedItemPath)
     }
 
     public async submitChange() {
+        const selectedContext = (this.byId("availableAttributesList") as List).getSelectedItem().getBindingContext()
+
         if (this.params.submitCallback) {
-            this.params.submitCallback({
-                path: "results/2"
-            })
+            this.params.submitCallback(selectedContext?.getPath().slice(1))
         }
         this.closePopup()
+    }
+
+    private setSelectedItem(selectedItemPath: String) {
+        const list = this.byId("availableAttributesList") as List
+        const selectedAttributeValue = this.getModel().getProperty(`/${selectedItemPath}`) as String
+
+        const selectedListItem = list
+            .getItems()
+            .find(
+                (listItem: StandardListItem) =>
+                    listItem?.getBindingContext()?.getObject("value/value") === selectedAttributeValue
+            ) as StandardListItem
+
+        list.setSelectedItem(selectedListItem)
     }
 }
